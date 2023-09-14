935257bd87e13c7821195d9b4b514ef8b353cf7c
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-09-14 16:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 //your code here
 
+let arr = [
+  "The Virupaksha Temple", // "Virupaksha Temple"
+  "a Victoria Memorial", //"Victoria Memorial"
+  "an Tajmahal", // "Tajmahal"
+];
+
+let articleLessArray = [];
+/* ["Virupaksha Temple", "Victoria Memorial", "Tajmahal" ]
+{
+    "Virupaksha Temple": "The Virupaksha Temple",
+    "Victoria Memorial": "a Victoria Memorial",
+    "Tajmahal": "an Tajmahal"
+}
+*/
+let mp = {};
+// {aricleLessItem: ""}
+
+for (let i = 0; i < arr.length; i++) {
+  let articleLessItem = arr[i].replace(regexp, "").trim();
+  articleLessItem = articleLessItem.replace(/  /g, " "); // remove two spaces with a single space
+  articleLessArray.push(articleLessItem);
+  mp[articleLessItem] = arr[i];
+  // for i = 0 arr[i] = "The Virupaksha Temple", articlLessItem = "Virupaksha Temple"
+
+  // mp = { "Virupaksha Temple": "The Virupaksha Temple" }
+}
+// articleLessArray = ["Virupaksha Temple",  "Victoria Memorial", "Tajmahal" ]
+//
+articleLessArray.sort();
+
+let ans = [];
+
+for (let i of articleLessArray) {
+  ans.push(mp[i]);
+}
+
+console.log(ans);
